4601a322e7423005d9b1944666b8e3a6d4f5fad4
muramoya <tash1.0713@gmail.com>
2017-10-18 12:25:28 +0900

    add TimeAttack

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# devtools
+# TimeAttack 使い方
+
+```php
+<?php
+
+//[optional]log_name
+//[optional]/log/path/hoge.log(default /tmp/timeattack.log)
+$timeAttack = new \DevTools\TimeAttack('log_name', '/log/path');
+
+//計測スタート
+$timeAttack->start(true);
+
+...
+//区間タイム測定
+//$timeAttack->start(true)の時 → start - splitまでの計測時間とsplit-split間の区間タイムを表示
+//$timeAttack->start(false)の時 → start - splitまでの計測時間のみ表示
+//[optional] label
+$timeAttack->split('label');
+
+...
+//計測終了
+$timeAttack->finish();
+```
---
+++ src/TimeAttack.php
@@ -0,0 +1,77 @@
+<?php
+namespace DevTools;
+
+use Monolog\Formatter\LineFormatter;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+
+class TimeAttack
+{
+	private $start;
+	private $finish;
+	private $splitTimeFrom;
+	private $splitTimeTo;
+	private $isShowSectionTime;
+	/**
+	 * @var Logger
+	 */
+	private $logger;
+
+	public function __construct($name = null, $logPath = '/tmp/timeattack.log')
+	{
+		$output = "[%datetime%] %message%\n";
+		$format = new LineFormatter($output);
+		$stream = new StreamHandler($logPath, Logger::INFO);
+		$stream->setFormatter($format);
+		$this->logger = new Logger($name, $stream);
+	}
+
+	public function start($showSection = false)
+	{
+		$this->init();
+		if($showSection) $this->isShowSectionTime = true;
+		$this->start = microtime(true);
+		$this->splitTimeFrom = microtime(true);
+		$this->output('Start');
+	}
+
+	public function split($label = null)
+	{
+		$start = $this->start;
+		$this->splitTimeTo = microtime(true);
+		$res = round($this->splitTimeTo - $start, 2);
+		$ret = 'Temporary time ' . $res . ' sec';
+		if($label)
+		{
+			$ret = $label . $ret;
+		}
+		if($this->isShowSectionTime)
+		{
+			$sectionTime = round($this->splitTimeTo - $this->splitTimeFrom, 2);
+			$ret .= "\n" . 'Section Time: ' . $sectionTime . ' sec';
+			$this->splitTimeFrom = microtime(true);
+		}
+		$this->output($ret);
+	}
+
+	public function finish()
+	{
+		$this->finish = microtime(true);
+		$ret = 'Result ' . $this->result() . ' sec' . "\n";
+		$ret .= 'Finish';
+		$this->output($ret);
+	}
+
+	private function output($val = null)
+	{
+		$this->logger->info($val);
+	}
+
+	private function result()
+	{
+		$start = $this->start;
+		$finish = $this->finish;
+		$res = round($finish - $start, 2);
+		return $res;
+	}
+}
