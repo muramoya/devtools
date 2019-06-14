52156e4e800d66e404304e324fe1d936cc940265
sakaitakuya <sakaitakuya@team-lab.com>
2019-06-14 15:37:36 +0900

    メソッドをstatic化;ちょっと修正

--- src/TimeAttack.php
+++ src/TimeAttack.php
@@ -7,72 +7,138 @@ use Monolog\Logger;
 
 class TimeAttack
 {
-	private $start;
-	private $finish;
-	private $splitTimeFrom;
-	private $splitTimeTo;
-	private $isShowSectionTime;
+    /**
+     * @var float
+     */
+	private static $start;
+
+    /**
+     * @var float
+     */
+	private static $finish;
+
+    /**
+     * @var float
+     */
+	private static $splitTimeFrom;
+
+    /**
+     * @var float
+     */
+	private static $splitTimeTo;
+
+    /**
+     * @var bool
+     */
+	private static $isShowSectionTime;
+
+    /**
+     * @var bool
+     */
+	private static $doesOutputFile = false;
+
+    /**
+     * @var int
+     */
+	private static $roundPrecision = 2;
+
 	/**
 	 * @var Logger
 	 */
-	private $logger;
+	private static $logger;
 
-	public function __construct($name = null, $logPath = '/tmp/timeattack.log')
-	{
-		$output = "[%datetime%] [%channel%] %message%\n";
+    /**
+     * @var string
+     */
+    private static $logLevel = 'info';
+
+
+    private const LOG_LEVELS = [
+        'debug',
+        'info',
+        'notice',
+        'warn',
+        'warning',
+        'err',
+        'error',
+        'crit',
+        'critical',
+        'alert',
+        'emerg',
+        'emergency',
+    ];
+
+    public static function enableFileOutput(string $filePath = null, string $outputFormat = null, string $logName = null): void
+    {
+        self::$doesOutputFile = true;
+        $filePath = $filePath ?? '/tmp/timeattack.log';
+        $output = $outputFormat ?? "[%datetime%] [%channel%] %message%\n";
 		$format = new LineFormatter($output);
-		$stream = new StreamHandler($logPath, Logger::INFO);
+		$stream = new StreamHandler($filePath, Logger::INFO);
 		$stream->setFormatter($format);
-		$this->logger = new Logger($name);
-		$this->logger->pushHandler($stream);
+		self::$logger = new Logger($logName);
+		self::$logger->pushHandler($stream);
+    }
+
+    public static function changeRoundPrecision(int $precision): void
+    {
+        self::$roundPrecision = $precision;
+    }
+
+    public static function setLogLevel(string $logLevel): void
+    {
+        if (self::$doesOutputFile) {
+            if (!in_array($logLevel, self::LOG_LEVELS, true)) throw new \Exception('setLogLevel: Invalid log level.');
+            self::$logLevel = $logLevel;
+        }
+    }
+
+	public static function start(bool $showSection = false): string
+	{
+		self::$isShowSectionTime = $showSection;
+		self::$start = microtime(true);
+		self::$splitTimeFrom = microtime(true);
+		return self::output('Start');
 	}
 
-	public function start($showSection = false)
+	public static function split(string $label = null): string
 	{
-		if($showSection) $this->isShowSectionTime = true;
-		$this->start = microtime(true);
-		$this->splitTimeFrom = microtime(true);
-		$this->output('Start');
-	}
-
-	public function split($label = null)
-	{
-		$start = $this->start;
-		$this->splitTimeTo = microtime(true);
-		$res = round($this->splitTimeTo - $start, 2);
+		$start = self::$start;
+		self::$splitTimeTo = microtime(true);
+		$res = round(self::$splitTimeTo - $start, self::$roundPrecision);
 		$ret = 'Temporary time: ' . $res . ' sec';
-		if($label)
-		{
+		if($label) {
 			$ret = '[' . $label . '] ' . $ret;
 		}
-		if($this->isShowSectionTime)
-		{
-			$sectionTime = round($this->splitTimeTo - $this->splitTimeFrom, 2);
+
+		if(self::$isShowSectionTime) {
+			$sectionTime = round(self::$splitTimeTo - self::$splitTimeFrom, self::$roundPrecision);
 			$ret .= '  Section Time: ' . $sectionTime . ' sec';
-			$this->splitTimeFrom = microtime(true);
+			self::$splitTimeFrom = microtime(true);
 		}
-		$this->output($ret);
+
+		return self::output($ret);
 	}
 
-	public function finish()
+	public static function finish(): string
 	{
-		$this->finish = microtime(true);
-		$ret = 'Result ' . $this->result() . ' sec' . "\n";
-		$this->output($ret);
-		$ret = 'Finish';
-		$this->output($ret);
+		self::$finish = microtime(true);
+		$ret = 'Result ' . self::result() . ' sec' . "\n" . 'Finish';
+		return self::output($ret);
 	}
 
-	private function output($val = null)
+	private static function output($val = null): string
 	{
-		$this->logger->info($val);
+	    if (self::$doesOutputFile) {
+	        $method = self::$logLevel;
+            self::$logger->$method($val);
+        }
+
+	    return $val;
 	}
 
-	private function result()
+	private static function result(): float
 	{
-		$start = $this->start;
-		$finish = $this->finish;
-		$res = round($finish - $start, 2);
-		return $res;
+		return round(self::$finish - self::$start, self::$roundPrecision);
 	}
 }
